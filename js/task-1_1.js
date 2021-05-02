e5e702f3b86a86437a3f16135a8ac97941bacce8
Pavlo Zub <zu10@ukr.net>
2021-05-02 18:55:42 +0300

    work
    task 1_1. 1_2

---
+++ js/task-1_1.js
@@ -0,0 +1,9 @@
+// "use strict";
+
+const name = 'Генератор захисного поля';
+let prise = 1000;
+console.log(`Обрано «${name}», ціна за штуку ${prise} кредитів`);
+
+prise = 2000;
+
+console.log(`Обрано «${name}», ціна за штуку ${prise} кредитів`);
---
+++ js/task-1_2.js
@@ -0,0 +1,18 @@
+const total = 100;
+console.log(total);
+let ordered = 20;
+console.log(ordered);
+
+console.log(total >= ordered ? "Замовлення оформлено, з вами зв'яжеться менеджер" : "На складі недостатньо товарів!");
+
+console.log(total);
+ordered = 50;
+console.log(ordered);
+
+console.log(total >= ordered ? "Замовлення оформлено, з вами зв'яжеться менеджер" : "На складі недостатньо товарів!");
+
+console.log(total);
+ordered = 130;
+console.log(ordered);
+
+console.log(total >= ordered ? "Замовлення оформлено, з вами зв'яжеться менеджер" : "На складі недостатньо товарів!");
